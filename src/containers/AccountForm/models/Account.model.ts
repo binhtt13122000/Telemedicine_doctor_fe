1a6d51f00695bd665066c3741822520824fafcd2
chloe-nguyen <chloenguyen1206@gmail.com>
2021-10-18 23:38:37 +0700

    NgocNTH: create account form

---
+++ src/containers/AccountForm/models/Account.model.ts
@@ -0,0 +1,15 @@
+export type Account = {
+    email: string;
+    firstName: string;
+    lastName: string;
+    streetAddress: string;
+    locality: string;
+    ward: string;
+    city: string;
+    postalCode: string;
+    phone: string;
+    avatar: string;
+    dob: string;
+    isMale: string | boolean;
+    roleId: number;
+};
